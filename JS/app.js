e41a765acf966c80983319c0d13972e314c9a439
Andrei Boar <boara.developeraddress@gmail.com>
2023-12-26 09:51:58 +0200

    Desk work.

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,9 @@
+let logInButton = document.getElementById('logInButton');
+let signUpButton = document.getElementById('signUpButton');
 
+logInButton.addEventListener("click", function(e) {
+    location.reload;
+});
 
 
 
